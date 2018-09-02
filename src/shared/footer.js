9806c771f78f27ac844924ff04db2e2e95bb5807
rodneystrong <akang2@gmail.com>
2018-09-01 19:17:10 -0700

    created the Footer component

---
+++ src/shared/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          This is where the copyright info usually goes, but i don't care if you
+          jack my shit. You can't innovate like me cause I was trained by Zach
+          Silveira.
+        </div>
+      </footer>
+    );
+  }
+}
