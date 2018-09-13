6ff28a6b6ef27932f1f9488b2137ae925218c1f9
rodneystrong <akang2@gmail.com>
2018-09-12 19:53:35 -0700

    finally got state working to display in console

--- src/shared/input.js
+++ src/shared/input.js
@@ -3,12 +3,21 @@ import React from 'react';
 export default class Input extends React.Component {
   constructor() {
     super();
+    this.state = {
+      searchInput: '',
+    };
   }
 
   //gotta create a function that captures the text input
   //then put it into the console
+  inputChange = event => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+    console.log(this.state.searchInput);
+  };
 
   render() {
-    return <input onChange={console.log(this.value)} />;
+    return <input name="randomInput" onChange={this.inputChange} />;
   }
 }
